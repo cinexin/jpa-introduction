11da2628cb06b191c4eaa1335953ea0de0218df8
cinexin <migueguillamon@gmail.com>
2020-03-07 12:31:31 +0100

    Initial commit Basic Sprint Boot project structure

---
+++ src/main/kotlin/org/cinexin/learning/jpa/jpaintroduction/JpaIntroductionApplication.kt
@@ -0,0 +1,11 @@
+package org.cinexin.learning.jpa.jpaintroduction
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JpaIntroductionApplication
+
+fun main(args: Array<String>) {
+	runApplication<JpaIntroductionApplication>(*args)
+}
---
+++ src/test/kotlin/org/cinexin/learning/jpa/jpaintroduction/JpaIntroductionApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.cinexin.learning.jpa.jpaintroduction
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JpaIntroductionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
